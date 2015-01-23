3b7d687ab481e36a11b562bd1aa05338f5e316c7
Carolyn Evans <carolyn.evans@enom.com>
2015-01-22 22:39:24 -0800

    Removed a diagnostic line from models.

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -64,7 +64,6 @@ class User(Base):
     def by_username(cls, username, session=None):
         if session is None:
             session = DBSession
-        x=session.query(cls).filter(cls.username==username).first()
         return session.query(cls).filter(cls.username==username).first()
 
     def verify_password(self, password):
